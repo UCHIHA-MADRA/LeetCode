e747bc2cd71a3308fe63dd9858b33af0e3da6119
UCHIHAMADRA <68822532+UCHIHA-MADRA@users.noreply.github.com>
2023-08-23 02:27:33 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (44.02%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string merged;
-        int i = 0, j = 0;
+       string merged = "";
+    
+    int i = 0, j = 0;
     while (i < word1.length() || j < word2.length()) {
         if (i < word1.length()) {
-            merged.push_back(word1[i]);
+            merged += word1[i];
             i++;
         }
         
         if (j < word2.length()) {
-            merged.push_back(word2[j]);
+            merged += word2[j];
             j++;
         }
     }
     
-    return merged;
-    }
+    return merged;}
 };
     
