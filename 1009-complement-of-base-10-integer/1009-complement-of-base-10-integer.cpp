add860c96a09f7b33f16c77bd038887e92744ca1
UCHIHAMADRA <68822532+UCHIHA-MADRA@users.noreply.github.com>
2023-08-30 21:44:14 +0530

    Time: 2 ms (42.99%), Space: 6 MB (14.64%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        if (n == 0) {
+        if (n == 0) 
 return 1; // Special case for input 0
-}
+
    int mask =0;
         int m = n;
         while(m)
